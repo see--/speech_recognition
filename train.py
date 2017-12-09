9c34cf201c97a0129ab2cf2013ad63b57a25a690
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2017-12-09 09:58:43 +0100

    Training 41

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def data_gen(audio_processor, sess,
 
 
 def lr_schedule(ep):
-  base_lr = 0.0003
+  base_lr = 0.001
   if ep <= 20:
     return base_lr
   elif 20 < ep <= 30:
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     data_dirs.append('data/pseudo/audio')
   compute_mfcc = False
   sample_rate = 16000
-  batch_size = 100
+  batch_size = 160
   classes = get_classes(wanted_only=False, extend_reversed=False)
   model_settings = prepare_model_settings(
       label_count=len(prepare_words_list(classes)), sample_rate=sample_rate,
@@ -99,9 +99,9 @@ if __name__ == '__main__':
               wanted_words=prepare_words_list(get_classes(wanted_only=True)),
               all_words=prepare_words_list(classes),
               label2int=ap.word_to_index),
-          TensorBoard(log_dir='logs_040'),
+          TensorBoard(log_dir='logs_041'),
           ModelCheckpoint(
-              'checkpoints_040/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')])
+              'checkpoints_041/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')])
 
   eval_res = model.evaluate_generator(
       val_gen, ap.set_size('validation') // batch_size)
