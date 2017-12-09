b175d370915ce0aff039101543051bc51a3bda91
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2017-12-09 14:52:09 +0100

    Training 43

--- train.py
+++ train.py
@@ -52,12 +52,12 @@ if __name__ == '__main__':
   sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
   K.set_session(sess)
   data_dirs = ['data/train/audio']
-  add_pseudo = False
+  add_pseudo = True
   if add_pseudo:
     data_dirs.append('data/pseudo/audio')
   compute_mfcc = False
   sample_rate = 16000
-  batch_size = 100
+  batch_size = 384
   classes = get_classes(wanted_only=False, extend_reversed=False)
   model_settings = prepare_model_settings(
       label_count=len(prepare_words_list(classes)), sample_rate=sample_rate,
@@ -82,17 +82,17 @@ if __name__ == '__main__':
   model.fit_generator(
       train_gen, ap.set_size('training') // batch_size,
       epochs=100, verbose=1, callbacks=[
-          ReduceLROnPlateau(monitor='val_categorical_accuracy', mode='max',
-                            factor=0.5, patience=2, verbose=1),
           ConfusionMatrixCallback(
               val_gen,
               ap.set_size('validation') // batch_size,
               wanted_words=prepare_words_list(get_classes(wanted_only=True)),
               all_words=prepare_words_list(classes),
               label2int=ap.word_to_index),
-          TensorBoard(log_dir='logs_042'),
+          TensorBoard(log_dir='logs_043'),
           ModelCheckpoint(
-              'checkpoints_042/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')])
+              'checkpoints_043/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5'),
+          ReduceLROnPlateau(monitor='val_categorical_accuracy', mode='max',
+                            factor=0.5, patience=3, verbose=1)])
 
   eval_res = model.evaluate_generator(
       val_gen, ap.set_size('validation') // batch_size)
