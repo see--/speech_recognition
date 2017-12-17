d3d5380c04bd15cf5c3f6c67b797ef394efd2de3
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2017-12-17 20:43:10 +0100

    Training 84: With residual connections

--- model.py
+++ model.py
@@ -725,24 +725,26 @@ def conv_1d_time_sliced_model(input_size=16000, num_classes=11):
         use_bias=False)
     return x
 
-  def _residual_block(x, num_filters, k):
-    residual = _context_conv(x, num_filters, k, padding='same')
-    residual = _context_conv(residual, num_filters, k, padding='same')
+  def _residual_reduce_block(x, num_filters, k):
+    residual = Conv1D(num_filters, 1, strides=2, use_bias=False,
+                      padding='same')(x)
+    residual = BatchNormalization()(residual)
+
+    x = _reduce_conv(x, num_filters, k, padding='same')
+    x = _context_conv(x, num_filters, k, padding='same')
+
     x = Add()([x, residual])
     return x
 
-  x = Reshape([400, 40])(x)
+  x = Lambda(lambda x: time_slice_stack(x, 40))(x)
   x = _reduce_conv(x, 64, 5)  # 250
   x = _context_conv(x, 64, 3)
-  x = _reduce_conv(x, 128, 3)  # 125
-  x = _context_conv(x, 128, 3)
-  x = _reduce_conv(x, 256, 3)  # 64
-  x = _context_conv(x, 256, 3)
-  x = _reduce_conv(x, 384, 3)  # 32
-  x = _context_conv(x, 384, 3)
-  x = _reduce_conv(x, 448, 3)  # 16
-  x = _context_conv(x, 448, 3)
-  x = _reduce_conv(x, 512, 3)  # 4
+  x = _residual_reduce_block(x, 96, 3)
+  x = _residual_reduce_block(x, 128, 3)
+  x = _residual_reduce_block(x, 256, 3)
+  x = _residual_reduce_block(x, 320, 3)
+  x = _residual_reduce_block(x, 384, 3)
+  x = _residual_reduce_block(x, 448, 3)
   x = GlobalAveragePooling1D()(x)
   x = Dropout(0.3)(x)
   x = Dense(num_classes, activation='softmax')(x)
@@ -750,7 +752,7 @@ def conv_1d_time_sliced_model(input_size=16000, num_classes=11):
 
   model = Model(input_layer, x, name='conv_1d_time_sliced')
   model.compile(
-      optimizer=keras.optimizers.RMSprop(lr=5e-4),
+      optimizer=keras.optimizers.RMSprop(lr=2e-3),
       loss=keras.losses.categorical_crossentropy,
       metrics=[keras.metrics.categorical_accuracy])
   return model
