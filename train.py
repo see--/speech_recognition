a131d5a7b7051251e1912e41d400c4c8a2450fa7
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2017-12-25 11:43:11 +0100

    Training 115: No pseudo labels

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def data_gen(audio_processor, sess,
              background_frequency=0.5, background_volume_range=0.2,
              foreground_frequency=0.5, foreground_volume_range=0.2,
              time_shift_frequency=0.5, time_shift_range=[-2000, 0],
-             mode='validation', pseudo_frequency=0.33):
+             mode='validation', pseudo_frequency=0.0):
   offset = 0
   if mode != 'training':
     background_frequency = 0.0
@@ -57,7 +57,7 @@ if __name__ == '__main__':
   sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
   K.set_session(sess)
   data_dirs = ['data/train/audio']
-  add_pseudo = True
+  add_pseudo = False
   if add_pseudo:
     data_dirs.append('data/pseudo/audio')
   output_representation = 'raw'
@@ -89,8 +89,8 @@ if __name__ == '__main__':
           label2int=ap.word_to_index),
       ReduceLROnPlateau(monitor='val_categorical_accuracy', mode='max',
                         factor=0.5, patience=4, verbose=1),
-      TensorBoard(log_dir='logs_114'),
-      ModelCheckpoint('checkpoints_114/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')]
+      TensorBoard(log_dir='logs_115'),
+      ModelCheckpoint('checkpoints_115/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')]
   model.fit_generator(
       train_gen, steps_per_epoch=ap.set_size('training') // batch_size,
       epochs=200, verbose=1, callbacks=callbacks)
