857ba9e820a8f6bac1e6769e5344c8a77e46a20e
Steffen Eberbach <steffen.eberbach@student.kit.edu>
2018-01-03 00:10:02 +0100

    Training 164: 12 classes & pseudo

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
   sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
   K.set_session(sess)
   data_dirs = ['data/train/audio']
-  add_pseudo = False
+  add_pseudo = True
   if add_pseudo:
     data_dirs.append('data/pseudo/audio')
   output_representation = 'raw'
@@ -51,7 +51,7 @@ if __name__ == '__main__':
       model_settings['fingerprint_size'] if output_representation != 'raw' else model_settings['desired_samples'],  # noqa
       num_classes=model_settings['label_count'],
       **model_settings)
-  embed()
+  # embed()
   callbacks = [
       ConfusionMatrixCallback(
           val_gen, ap.set_size('validation') // batch_size,
@@ -63,7 +63,7 @@ if __name__ == '__main__':
       TensorBoard(log_dir='logs_164'),
       ModelCheckpoint('checkpoints_164/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5')]
   model.fit_generator(
-      train_gen, steps_per_epoch=ap.set_size('training') // batch_size,
+      train_gen, steps_per_epoch=ap.set_size('training') // (batch_size * 0.5),
       epochs=100, verbose=1, callbacks=callbacks)
 
   eval_res = model.evaluate_generator(
